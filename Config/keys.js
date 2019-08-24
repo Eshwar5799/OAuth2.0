5872aa094376803163ee91657cd04dbea1911762
Eshwar5799 <eshwarsv998@gmail.com>
2019-08-24 23:41:51 +0530

    Twitter token error:

--- Config/keys.js
+++ Config/keys.js
@@ -12,7 +12,7 @@ module.exports={
         consumerkey:"DfbUqrs9sauV8Ht9pNSEipWGG",
         clientSecret:"ayDAzWzZMECISBsV4GPPoZD78Kh7DXc9BDUzZ7EwJTNROSfWHo",
         host:"http://localhost:9000/",
-        callbackURL:"http://localhost:9000/auth/twitter/redirect"
+        callbackURL:"http://127.0.0.1:9000/auth/twitter/redirect"
     },
     linkedin:{
         clientID:"77ahlyciugvv3n",
--- app.js
+++ app.js
@@ -11,13 +11,7 @@ const port=9000;
 var session=require('express-session');
 
 // Express session for twitter
-app.use(session({
-    secret: 'keyboard cat',
-    resave: false,
-    saveUninitialized: true,
-    cookie: { secure: true }
-  }))
-
+app.use(session({ secret: 'keyboard cat', key: 'sid' ,cookie: { secure: true }}))
 
 app.use(passport.initialize())
 app.use(passport.session())
